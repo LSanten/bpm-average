724cfc978d9472f4270b2c3ff04569d7200bc63c
LSanten <leon.santen@icloud.com>
2019-07-11 16:46:11 -0400

    debug mode: counts time between pushs reliably
    todo: convert time into BPM and change screen to BPM (not debug)

--- RUN_bpm_counter/RUN_bpm_counter.ino
+++ RUN_bpm_counter/RUN_bpm_counter.ino
@@ -5,8 +5,6 @@
 
 #include<LiquidCrystal.h>
 
-
-
 // initalize the library with the numbers of the interface pins
 LiquidCrystal lcd(12,11,5,4,3,2);
 
@@ -14,7 +12,18 @@ LiquidCrystal lcd(12,11,5,4,3,2);
 
 int led1 = 9;
 int button = 10;
-int button_last_status; // will be set to 1 when button pressed; to 0 when not pressed
+int buttonLastStatus; // will be set to 1 when button pressed; to 0 when not pressed
+int maxInterval = 3000; // max time in milliseconds after which BPM resets itself
+int minInterval = 200; // min interval between button pushs in milliseconds
+
+uint32_t firstBeat = 0; // millis of first beat
+uint32_t lastBeat = 0; // millis of last beat
+uint32_t beatCount = 0; // number of how many times one has pressed the button
+
+float intervalTime;
+float bpm;
+
+
 
 void setup() {
   // put your setup code here, to run once:
@@ -25,11 +34,19 @@ void setup() {
   // set up the LCD's number of columns and rows
   lcd.begin(16, 2);
 
+  /* LCD Screen Print Example
   lcd.print("avg. BPM:");
   lcd.setCursor(10, 0); // (column, row) --> it starts with 0
   lcd.print("123.55");
   lcd.setCursor(0, 1);
   lcd.print("----------------");
+  */
+  lcd.setCursor(0, 0);
+  lcd.print("BPM Counter");
+  lcd.setCursor(0, 1);
+  lcd.print("----------------");
+  delay(2500);
+  lcd.clear();
 
 }
 
@@ -40,20 +57,77 @@ void loop() {
   if (digitalRead(button) == HIGH) //if button is pressed (LOW), led will light up
   {
     digitalWrite(led1, LOW);
-    button_last_status = 0;
+    buttonLastStatus = 0;
   }
   else 
   {
     digitalWrite(led1, HIGH);
     
   
-    if (button_last_status == 0) // print millis when first button push in a row
+    if (buttonLastStatus == 0) // print millis when first button push in a row
     {
-      uint32_t t1 = millis();  
-      Serial.println(t1); // print millis since program started
-    }
+      uint32_t t1 = millis();
+
+      if (t1 - lastBeat > maxInterval and lastBeat != 0 ) //resets BPM counter as soon as maxInterval is reached
+      {
+        // reset BPM variables
+        firstBeat = 0;
+        lastBeat = 0;
+        beatCount = 0;
+
+        // reset Screen
+        lcd.clear();
+      }
+
+      
+
+      if (t1 - lastBeat < minInterval) // let's counter only increment when interval between push is smaller than minimalInterval
+      {
+        
+      }
+      else
+      {
+        beatCount++; 
+ 
+
+        if (beatCount == 1) // sets first button push time to firstBeat, then last one to lastBeat
+        {
+          firstBeat = t1;
+        }
+        else if (beatCount > 1)
+        {
+          lastBeat = t1;
+          intervalTime = (lastBeat - firstBeat)/(beatCount - 1);
+          
+        }
   
-    button_last_status = 1; // sets button status to 1 so that Serial doesn't print continuously but only first time when button pressed
+        // Serial Prints
+        Serial.println(t1); // print millis since program started
+  
+        // LCD Prints
+  
+        
+  
+        //Debugging Screen
+        lcd.setCursor(0, 0); //firstBeat
+        lcd.print(firstBeat);
+        
+        lcd.setCursor(0, 1); //lastB
+        lcd.print(lastBeat);
+  
+        lcd.setCursor(10, 0); //t1
+        lcd.print(intervalTime);
+    
+        //lcd.setCursor(8, 0); //t1
+        //lcd.print(t1);
+    
+        lcd.setCursor(14, 1); // print beatCount on LCD screen
+        lcd.print(beatCount);
+      }
+      
+    }
+      
+    buttonLastStatus = 1; // sets button status to 1 so that Serial doesn't print continuously but only first time when button pressed
   }
   
 
